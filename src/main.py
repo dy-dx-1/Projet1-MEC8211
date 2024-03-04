af09fa417b27ccc7bc1c4c240aee1a6b2fdc23dd
dy-dx-1 <nic19sarbe@gmail.com>
2024-03-04 10:11:47 -0500

    reformatted main()

--- src/main.py
+++ src/main.py
@@ -18,7 +18,7 @@ class Data:
     N = 5
     domaine = np.linspace(0, ro, N)
 
-def main(): 
+def Devoir1(): 
     params = Data() 
     n_values=[5,500]
     ### Solution analytique 
@@ -27,53 +27,49 @@ def main():
     dom_analytique = np.linspace(0, params.ro, 100) 
     C_exact_domaine = C_exact(dom_analytique) # Concentration exacte évaluée sur le domaine de discrétisation 
 
-    # ### Question D: Profil de concentration stationnaire avec S constant et coeff concentration ordre 1 ; le tout avec derivée premiere ordre 1
-    # profil_S_constant_trans = solve.solveur_transitoire(params, consommation_constante=True, ordre_derive_premiere=1)
-    # profil_S_ordre1_trans = solve.solveur_transitoire(params, consommation_constante=False, ordre_derive_premiere=1)
-    # graphiques_D = [(params.domaine, profil_S_constant_trans, r"$S=8*10^{-9}$", ".-"), (params.domaine, profil_S_ordre1_trans, r"$S=k*C$", ".-")]
-    # show_graphs(f"Profil de concentration transitoire après 10 ans selon le type de source", "Position radiale [m]", r"Concentration [mol/$m^3$]", graphiques_D)
+    ### Question D: Profil de concentration stationnaire avec S constant et coeff concentration ordre 1 ; le tout avec derivée premiere ordre 1
+    profil_S_constant_trans = solve.solveur_transitoire(params, consommation_constante=True, ordre_derive_premiere=1)
+    profil_S_ordre1_trans = solve.solveur_transitoire(params, consommation_constante=False, ordre_derive_premiere=1)
+    graphiques_D = [(params.domaine, profil_S_constant_trans, r"$S=8*10^{-9}$", ".-"), (params.domaine, profil_S_ordre1_trans, r"$S=k*C$", ".-")]
+    show_graphs(f"Profil de concentration transitoire après 10 ans selon le type de source", "Position radiale [m]", r"Concentration [mol/$m^3$]", graphiques_D)
+    ### Question E: Comparaison entre sol stationnaire S constant et analytique, avec derive_premiere d'ordre 1
+    cas_a_resoudre = lambda: solve.solveur_stationnaire(params, consommation_constante=True, ordre_derive_premiere=1)
+    graphiques_E = generate_n_graphs(params, cas_a_resoudre, n_values)
+    graphiques_E.append((dom_analytique, C_exact_domaine, "Analytique avec N = 100", "-")) 
+    show_graphs("Profil obtenu numériquement et analytiquement pour une source constante et approx dérivée première ordre 1", "Position radiale [m]", r"Concentration [mol/$m^3$]",
+             graphiques_E)
+    ### Question E)b): Sur un même graphique les erreurs L1, L2 et L∞. 
+    Erreur_L1=[]
+    Erreur_L2=[]
+    Erreur_Linf=[]
+    for i in range(len(n_values)):
+        Erreur_L1.append(erreur_L1(graphiques_E[i][0],graphiques_E[i][1],C_exact(graphiques_E[i][0])))
+        Erreur_L2.append(erreur_L2(graphiques_E[i][0],graphiques_E[i][1],C_exact(graphiques_E[i][0])))
+        Erreur_Linf.append(erreur_Linf(graphiques_E[i][0],graphiques_E[i][1],C_exact(graphiques_E[i][0])))
+       
+    graphique_erreur("ordre 1",n_values,[Erreur_L1,Erreur_L2,Erreur_Linf])
+    erreur_de_convergence_observe(params,n_values,Erreur_L1,Erreur_L2,Erreur_Linf)
+    # Valeurs exactes ne changent évidamment pas 
+    cas_a_resoudre = lambda: solve.solveur_stationnaire(params, consommation_constante=True, ordre_derive_premiere=2)
+    graphiques_D = generate_n_graphs(params, cas_a_resoudre, n_values)
+    graphiques_D.append((dom_analytique, C_exact_domaine, "Analytique avec N = 100", "-")) 
+    show_graphs("Profil obtenu numériquement et analytiquement pour une source constante et approx dérivée première ordre 2", "Position radiale [m]", r"Concentration [mol/$m^3$]",
+             graphiques_D)
+    # Question F)a)
+    Erreur_L1=[]
+    Erreur_L2=[]
+    Erreur_Linf=[]
+    for i in range(len(n_values)):
+         Erreur_L1.append(erreur_L1(graphiques_D[i][0],graphiques_D[i][1],C_exact(graphiques_D[i][0])))
+         Erreur_L2.append(erreur_L2(graphiques_D[i][0],graphiques_D[i][1],C_exact(graphiques_D[i][0])))
+         Erreur_Linf.append(erreur_Linf(graphiques_D[i][0],graphiques_D[i][1],C_exact(graphiques_D[i][0])))
+      
+    #graphique_erreur("ordre 2",n_values,[Erreur_L1,Erreur_L2,Erreur_Linf]) 
+    #erreur_de_convergence_observe(params,n_values,Erreur_L1,Erreur_L2,Erreur_Linf)
+    return None 
 
-    # ### Question E: Comparaison entre sol stationnaire S constant et analytique, avec derive_premiere d'ordre 1
-    # cas_a_resoudre = lambda: solve.solveur_stationnaire(params, consommation_constante=True, ordre_derive_premiere=1)
-    # graphiques_E = generate_n_graphs(params, cas_a_resoudre, n_values)
-    # graphiques_E.append((dom_analytique, C_exact_domaine, "Analytique avec N = 100", "-")) 
-    # show_graphs("Profil obtenu numériquement et analytiquement pour une source constante et approx dérivée première ordre 1", "Position radiale [m]", r"Concentration [mol/$m^3$]",
-    #         graphiques_E)
-
-    # ### Question E)b): Sur un même graphique les erreurs L1, L2 et L∞. 
-    # Erreur_L1=[]
-    # Erreur_L2=[]
-    # Erreur_Linf=[]
-    # for i in range(len(n_values)):
-    #     Erreur_L1.append(erreur_L1(graphiques_E[i][0],graphiques_E[i][1],C_exact(graphiques_E[i][0])))
-    #     Erreur_L2.append(erreur_L2(graphiques_E[i][0],graphiques_E[i][1],C_exact(graphiques_E[i][0])))
-    #     Erreur_Linf.append(erreur_Linf(graphiques_E[i][0],graphiques_E[i][1],C_exact(graphiques_E[i][0])))
-        
-    # graphique_erreur("ordre 1",n_values,[Erreur_L1,Erreur_L2,Erreur_Linf])
-    # erreur_de_convergence_observe(params,n_values,Erreur_L1,Erreur_L2,Erreur_Linf)
-    # # Valeurs exactes ne changent évidamment pas 
-    # cas_a_resoudre = lambda: solve.solveur_stationnaire(params, consommation_constante=True, ordre_derive_premiere=2)
-    # graphiques_D = generate_n_graphs(params, cas_a_resoudre, n_values)
-    # graphiques_D.append((dom_analytique, C_exact_domaine, "Analytique avec N = 100", "-")) 
-    # show_graphs("Profil obtenu numériquement et analytiquement pour une source constante et approx dérivée première ordre 2", "Position radiale [m]", r"Concentration [mol/$m^3$]",
-    #         graphiques_D)
-    # # Question F)a)
-
-    # Erreur_L1=[]
-    # Erreur_L2=[]
-    # Erreur_Linf=[]
-    # for i in range(len(n_values)):
-    #     Erreur_L1.append(erreur_L1(graphiques_D[i][0],graphiques_D[i][1],C_exact(graphiques_D[i][0])))
-    #     Erreur_L2.append(erreur_L2(graphiques_D[i][0],graphiques_D[i][1],C_exact(graphiques_D[i][0])))
-    #     Erreur_Linf.append(erreur_Linf(graphiques_D[i][0],graphiques_D[i][1],C_exact(graphiques_D[i][0])))
-        
-    # #graphique_erreur("ordre 2",n_values,[Erreur_L1,Erreur_L2,Erreur_Linf]) 
-    # #erreur_de_convergence_observe(params,n_values,Erreur_L1,Erreur_L2,Erreur_Linf)
-    # return None 
-    
-    """
-    Travail 2
-    """
+def Devoir2():
+    params = Data() 
     #Tracer la solution avec S non constant et ordre 2
     nb_annees = 100
     nb_jours = nb_annees*365.25
@@ -114,8 +110,11 @@ def main():
     plt.plot(params.domaine,Solution_MMS,label="MMS")
     plt.legend()
     plt.show()
-    
-    
+    return 
+
+def main(): 
+    Devoir2() 
+    return 
 
 if __name__=="__main__": 
     main() 
