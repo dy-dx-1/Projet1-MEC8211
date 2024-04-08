0bed98bc8ed0b48e39fdfa313e21cca84fd07c5b
Alexandre Carayon <alexandre.carayon@ensam.eu>
2024-04-08 16:04:37 -0400

    Graphiques Erreurs MMS

---
+++ Projet_Final/src/MMS/graphique_convergence.py
@@ -0,0 +1,124 @@
+import matplotlib.pyplot as plt 
+import matplotlib.gridspec as gr
+import numpy as np 
+from math import log
+
+def erreur_L1(domaine, results_numerique, results_analytique):
+    """
+    Fonction permettant de calculer l'erreur L1.
+
+    Parameters
+    ----------
+    data_instance : TYPE class
+        DESCRIPTION.
+    results : TYPE tableau numpy
+        DESCRIPTION.
+    tableau_f : TYPE tableau numpy
+        DESCRIPTION.
+
+    Returns
+    -------
+    erreur : float
+        Erreur L1 calculée.
+
+    """
+    erreur = 0
+    for i in range(len(domaine)):
+        for j in range(len(domaine)):
+            erreur += abs(results_numerique[i][j] - results_analytique[i][j])
+    return erreur/(len(domaine)**2)
+
+def erreur_L2(domaine, results_numerique, results_analytique):
+    """
+    Fonction permettant de calculer l'erreur L2.
+
+    Parameters
+    ----------
+    data_instance : TYPE class
+        DESCRIPTION.
+    results : TYPE tableau numpy
+        DESCRIPTION.
+    tableau_f : TYPE tableau numpy
+        DESCRIPTION.
+
+    Returns
+    -------
+    erreur : float
+        Erreur L2 calculée.
+
+    """
+    erreur = 0
+    for i in range(len(domaine)):
+        for j in range(len(domaine)):
+            erreur += (results_numerique[i][j] - results_analytique[i][j]) ** 2
+    return (erreur/len(domaine)) ** 0.5
+
+def erreur_Linf(domaine, results_numerique, results_analytique):
+    """
+    Fonction permettant de calculer l'erreur Linf.
+
+    Parameters
+    ----------
+    data_instance : TYPE class
+        DESCRIPTION.
+    results : TYPE tableau numpy
+        DESCRIPTION.
+    tableau_f : TYPE tableau numpy
+        DESCRIPTION.
+
+    Returns
+    -------
+    erreur : float
+        Erreur Linf calculée.
+
+    """
+    erreur = abs(results_numerique - results_analytique) # car results numérique est sous forme colonne et analytique sous forme ligne 
+    return np.amax(erreur)
+
+def graphique_convergence_erreurs(delta_vals:list, erreurs:list, type_delta:str):
+    """
+    Affiche un graphique de l'évolution de l'erreur en fonction de dt ou de dr.
+
+    Parameters
+    ----------        
+    delta_vals : list
+        Liste des valeurs de dt ou dr.
+    erreurs : list of lists
+        Liste des erreurs L1, L2, et Linf.
+    type_delta : 'dt' ou 'dr' 
+
+    Returns
+    -------
+    None
+    """
+    print(f"***Ordre de convergence {'SPATIAL' if type_delta=='dr' else 'TEMPOREL'} observé***")
+    for i, erreur in enumerate(erreurs): 
+        j = 'inf' if i==2 else str(i+1) 
+        coeffs = np.polyfit(np.log(delta_vals[-3:]), np.log(erreur[-3:]), 1)
+
+        ordre = coeffs[0] if j=='inf' else coeffs[0]
+        print(f"Erreur L{j} : {ordre:.4f}")
+
+        fit_function_log = lambda x: ordre * x + coeffs[1]
+        # Fonction de régression en termes originaux
+        fit_function = lambda x: np.exp(fit_function_log(np.log(x)))
+        plt.scatter(delta_vals, erreur, marker='o', color='b', label=f"Erreur L{j} ; Ordre: {ordre:.4f}")
+        plt.plot(delta_vals, fit_function(delta_vals), linestyle='--', color='r', label='Régression en loi de puissance')
+       
+        plt.legend() 
+        plt.grid(True)
+        plt.yscale('log')
+        plt.xscale('log')
+        if type_delta=="dr":
+            plt.ylabel('Erreur ')
+            plt.xlabel('dr [m]')
+            plt.title(f"Convergence de l'erreur L{j} selon le pas spatial")
+        elif type_delta=="dt":
+            plt.ylabel('Erreur ')
+            plt.xlabel('dt [s]')
+            plt.title(f"Convergence de l'erreur L{j} selon le pas de temps")
+        else:
+            print("type delta in graphique_convergence_erreurs not valid")
+            return 
+        plt.savefig(f'Convergence_de_l-erreur_L{j}.png')
+        plt.show() 
--- Projet_Final/src/MMS/main.py
+++ Projet_Final/src/MMS/main.py
@@ -2,6 +2,8 @@ import fonctions as f
 import numpy as np 
 import matplotlib.pyplot as plt 
 
+from  graphique_convergence import  graphique_convergence_erreurs,erreur_L1,erreur_L2,erreur_Linf
+
 class Parametres():
     u_inf = 1 
     R = 1 
@@ -57,7 +59,9 @@ def main():
     # pour faciliter l'analyse, on considèrera que nx = ny  
     noeuds_err = range(5, 55, 5) # valeurs qu'on imposera aux noeuds 
     erreurs_psi, erreurs_vitesses = list(), list() 
-
+    errL1=[]
+    errL2=[]
+    errLinf=[]
     for nb_noeuds in noeuds_err: 
         print("Calculating ", nb_noeuds)
         prm.nx = nb_noeuds
@@ -80,50 +84,70 @@ def main():
         erreur_relative_array = lambda arr_theo, arr_mdf: (np.average(abs(arr_theo-arr_mdf+1e-10)/(arr_theo+1e-10)))*100 # on perturbe de 1e-10 pour éviter /0
         erreurs_psi.append(erreur_relative_array(psi_analytique_mesh, psi_mdf_mesh)) # erreur relative en %
         erreurs_vitesses.append(erreur_relative_array(vitesses_ref, vitesses_mdf)) 
-    # Faisons le graphique 
-    fig, axs = plt.subplots(2,1) 
-    ax1, ax2 = axs[0], axs[1]
+        
+        domaine = [i for i in range(prm.nx)]
+        errL1.append(erreur_L1(domaine, psi_mdf_mesh, psi_analytique_mesh))
+        errL2.append(erreur_L2(domaine, psi_mdf_mesh, psi_analytique_mesh))
+        errLinf.append(erreur_Linf(domaine, psi_mdf_mesh, psi_analytique_mesh))
+        
+    erreurs=[errL1,errL2,errLinf]
+    # # Faisons le graphique 
+    # fig, axs = plt.subplots(2,1) 
+    # ax1, ax2 = axs[0], axs[1]
     
-    ax1.plot(noeuds_err, erreurs_psi, '.-g', label="Erreur sur $\psi$") 
-    ax2.plot(noeuds_err, erreurs_vitesses, '.-b', label="Erreur sur la vitesse") 
+    # ax1.plot(noeuds_err, erreurs_psi, '.-g', label="Erreur sur $\psi$") 
+    # ax2.plot(noeuds_err, erreurs_vitesses, '.-b', label="Erreur sur la vitesse") 
 
-    ax1.set_title(r"Erreur relative sur $\psi$ et les vitesses selon la taille du maillage")
-    ax1.set_ylabel(r"Erreur relative par rapport à la solution analytique[%]")
-    ax1.grid(True) 
-    ax1.legend() 
+    # ax1.set_title(r"Erreur relative sur $\psi$ et les vitesses selon la taille du maillage")
+    # ax1.set_ylabel(r"Erreur relative par rapport à la solution analytique[%]")
+    # ax1.grid(True) 
+    # ax1.legend() 
 
-    ax2.set_xlabel(r"Nombre de noeuds sur le bord $nx=ny$")
-    ax2.set_ylabel(r"Erreur relative par rapport à la solution analytique[%]")
-    ax2.grid(True) 
-    ax2.legend()    
-    plt.show() 
+    # ax2.set_xlabel(r"Nombre de noeuds sur le bord $nx=ny$")
+    # ax2.set_ylabel(r"Erreur relative par rapport à la solution analytique[%]")
+    # ax2.grid(True) 
+    # ax2.legend()    
+    # plt.show() 
     
-    # Maintenant faisons un graphique log log pour vérifier l'ordre de convergence 
-    fig, axs = plt.subplots(2,1) 
-    ax1, ax2 = axs[0], axs[1]
-    ax1.plot(noeuds_err, erreurs_psi, '.-g', label="Erreur sur $\psi$") 
-    ax2.plot(noeuds_err, erreurs_vitesses, '.-b', label="Erreur sur la vitesse") 
+    # # Maintenant faisons un graphique log log pour vérifier l'ordre de convergence 
+    # fig, axs = plt.subplots(2,1) 
+    # ax1, ax2 = axs[0], axs[1]
+    # ax1.plot(noeuds_err, erreurs_psi, '.-g', label="Erreur sur $\psi$") 
+    # ax2.plot(noeuds_err, erreurs_vitesses, '.-b', label="Erreur sur la vitesse") 
 
-    ax1.set_title(r"Erreur relative sur $\psi$ et les vitesses selon la taille du maillage")
-    ax1.set_ylabel(r"Erreur relative par rapport à la solution analytique[%]")
-    ax1.grid(True) 
-    ax1.legend() 
+    # ax1.set_title(r"Erreur relative sur $\psi$ et les vitesses selon la taille du maillage")
+    # ax1.set_ylabel(r"Erreur relative par rapport à la solution analytique[%]")
+    # ax1.grid(True) 
+    # ax1.legend() 
 
-    ax2.set_xlabel(r"Nombre de noeuds sur le bord $nx=ny$")
-    ax2.set_ylabel(r"Erreur relative par rapport à la solution analytique[%]")
-    ax2.grid(True) 
-    ax2.legend()    
+    # ax2.set_xlabel(r"Nombre de noeuds sur le bord $nx=ny$")
+    # ax2.set_ylabel(r"Erreur relative par rapport à la solution analytique[%]")
+    # ax2.grid(True) 
+    # ax2.legend()    
 
-    ax1.set_yscale("log") 
-    ax1.set_xscale("log")
-    ax2.set_yscale("log")
-    ax2.set_xscale("log")
-    noeuds_log = np.log(noeuds_err) 
-    erreurs_psi_log = np.log(erreurs_psi)
-    erreurs_vitesses_log = np.log(erreurs_vitesses)  
-    print(f"Pente du graphique log log (ordre de l'erreur): {np.polyfit(noeuds_log, erreurs_psi_log, 1)[0]:.0f}")
-    print(f"Pente du graphique log log (ordre de l'erreur): {np.polyfit(noeuds_log, erreurs_vitesses_log, 1)[0]:.0f}")
-    plt.show() 
+    # ax1.set_yscale("log") 
+    # ax1.set_xscale("log")
+    # ax2.set_yscale("log")
+    # ax2.set_xscale("log")
+    # noeuds_log = np.log(noeuds_err) 
+    # erreurs_psi_log = np.log(erreurs_psi)
+    # erreurs_vitesses_log = np.log(erreurs_vitesses)  
+    # print(f"Pente du graphique log log (ordre de l'erreur): {np.polyfit(noeuds_log, erreurs_psi_log, 1)[0]:.0f}")
+    # print(f"Pente du graphique log log (ordre de l'erreur): {np.polyfit(noeuds_log, erreurs_vitesses_log, 1)[0]:.0f}")
+    # plt.show() 
+    
+    #Graphiques de l'erreur
+    
+    r_min, r_max, theta_min, theta_max = prm.R, prm.R_ext, prm.theta_min, prm .theta_max
+    
+    dr = lambda nx: abs(r_max-r_min)/(nx-1)
+    dtheta = lambda ny: abs(theta_max-theta_min)/(ny-1)
+    tab=np.array([dr(i)*dtheta(i) for i in noeuds_err])
+    print(tab)
+
+    #erreurs=[np.array(erreurs_vitesses),np.array(erreurs_psi)]
+   
+    graphique_convergence_erreurs(tab,erreurs,'dr')
     
 if __name__=="__main__": 
     main() 
